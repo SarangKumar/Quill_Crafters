26fa0c69855c8927afdead74ab0d8dc1c806e2bd
SarangKumar <sarangkumar1578@gmail.com>
2023-11-22 12:55:17 +0530

    minor changes in ui

--- app/layout.js
+++ app/layout.js
@@ -8,17 +8,40 @@ const poppins = Poppins({ subsets: ['latin'], weight: ['400', '700'] });
 
 export const metadata = [
 	{
-		title: 'Quill Crafters - Creative Writing and More',
+		metadataBase: new URL('https://quillcrafters.vercel.app/'),
+		title: {
+			default: 'Quill Crafters - Creative Writing and More',
+			template: '%s | Quill Crafters',
+		},
 		description:
 			'Explore a world of creative writing, novels, poetry, and more at Quill Crafters. Join our community of writers and readers today.',
+		verification: {
+			google: 'google',
+		},
+		creator: 'Sarang',
+		authors: {
+			name: 'Sarang Kumar',
+			url: 'https://github.com/SarangKumar',
+		},
+		category: 'education',
+		keywords: [
+			'Next.js',
+			'Education',
+			'Quill Crafters',
+			'Fantacy',
+			'Comics',
+			'Novels',
+			'Action',
+			'Adventure',
+			'Stripe',
+			'Modern UI',
+		],
 	},
 	{
 		name: 'google-adsense-account',
 		content: 'ca-pub-9615068362549302',
 	},
 ];
-
-
 
 export default function RootLayout({ children }) {
 	return (
